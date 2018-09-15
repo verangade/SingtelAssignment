0464a8e7488fb9f1d93f3debf3ac3f40fe3cfa38
verangade <verangades@gmail.com>
2018-09-15 23:38:52 +0800

    2 c Model a Clownfish

---
+++ src/com/sta/animal/ClownFish.java
@@ -0,0 +1,15 @@
+package com.sta.animal;
+
+public class ClownFish extends Fish {
+	
+	public ClownFish() {
+		setBody("small");
+		setColor("orange");
+	}
+	
+	public void joke() {
+		System.out.println("I can Joke");
+	}
+
+
+}
